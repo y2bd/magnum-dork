64fcb5515f9467dbfb5e3519f0b06557f16e901e
Jason Lo <github@jasonlo.email>
2021-05-29 01:07:21 -0700

    Add gh-pages script

---
+++ scripts/gh-pages.ts
@@ -0,0 +1,16 @@
+var ghpages = require("gh-pages");
+
+ghpages.publish(
+  "public", // path to public directory
+  {
+    branch: "gh-pages",
+    repo: "https://github.com/y2bd/magnum-dork.git", // Update to point to your repository
+    user: {
+      name: "Jason Lo", // update to use your name
+      email: "github@jasonlo.email", // Update to use your email
+    },
+  },
+  () => {
+    console.log("Deploy Complete!");
+  }
+);
